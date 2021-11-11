c10fbedec2fdbcc05b4cfdde37e61fc5094b10ad
Austin Andika Tanujaya <austin.tanujaya@binus.edu>
2021-11-11 22:24:13 +0700

    Add Guest Header

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+
+    <link rel="stylesheet" href="css/layouts/master.css">
+    <link rel="stylesheet" href="css/layouts/navbarGuest.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+    <section class="navbar-section">
+        @include('layouts.navbarGuest')
+    </section>
+
+    <section>
+        @yield('body')
+    </section>
+</body>
+</html>
---
+++ resources/views/layouts/navbarGuest.blade.php
@@ -0,0 +1,21 @@
+
+<div class="navbar-container">
+    <div class="logo-container">
+        <a href="" class="logo"><span style="font-weight: bold; color: black">Game</span><span style="font-weight: bold; color: red">SLot</span></a>
+    </div>
+
+    <div class="search-bar-container">
+        <i class="fa fa-search" aria-hidden="true" style="color: grey"></i>
+        <input type="text" placeholder="Search" class="search-bar">
+    </div>
+
+    <div class="button-container">
+        <div class="button sign-in">
+            <a href="">Sign in</a>
+        </div>
+
+        <div class="button sign-up">
+            <a href="">Sign up</a>
+        </div>
+    </div>
+</div>
