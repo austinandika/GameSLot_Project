4f3e6c7130b701d427b4f7ee2caa4120f9a336a7
Austin Andika Tanujaya <austin.tanujaya@binus.edu>
2021-12-20 21:50:08 +0700

    Add authorization

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,6 @@ Route::get('/login', [LoginController::class, 'index'])->name('account.login')->
 Route::post('/login', [LoginController::class, 'authenticate'])->name('account.login');
 Route::get('/logout', [LoginController::class, 'logout'])->name('account.logout');
 
-
 Route::get('/register', [RegisterController::class, 'index'])->name('account.register')->middleware('guest');
 Route::post('/register', [RegisterController::class, 'createUser'])->name('account.register');
 
@@ -38,7 +37,7 @@ Route::prefix('game')->group(function () {
     Route::get('/{id}', [GameController::class, 'getGameDetail'])->name('game.detail');
 });
 
-Route::prefix('manage-game')->group(function () {
+Route::prefix('manage-game')->middleware('admin')->group(function () {
     Route::get('/', [GameController::class, 'getListGameDetail'])->name('manageGame.manage');
     Route::get('/add', [GameController::class, 'getAddGameView'])->name('manageGame.add');
     Route::post('/add', [GameController::class, 'addGame'])->name('manageGame.add');
@@ -47,52 +46,27 @@ Route::prefix('manage-game')->group(function () {
     Route::delete('/delete/{id}', [GameController::class, 'deleteGame'])->name('manageGame.delete');
 });
 
-Route::prefix('manage-genre')->group(function () {
+Route::prefix('manage-genre')->middleware('admin')->group(function () {
     Route::get('/', [GenreController::class, 'getListGenreDetail'])->name('manageGenre.manage');
     Route::get('/{id}', [GenreController::class, 'getGenreDetailUpdate'])->name('manageGenre.detail');
     Route::put('/update', [GenreController::class, 'updateGenre'])->name('manageGenre.update');
 });
 
-Route::prefix('cart')->group(function () {
-    Route::get('/', [CartController::class, 'getCart'])->name('cart.list')->middleware('auth');
-    Route::post('/add', [CartController::class, 'addToCart'])->name('cart.add')->middleware('auth');
-    Route::put('/update', [CartController::class, 'updateCart'])->name('cart.update')->middleware('auth');
-    Route::delete('/remove/{id}', [CartController::class, 'removeCart'])->name('cart.remove')->middleware('auth');
+Route::prefix('cart')->middleware('auth')->group(function () {
+    Route::get('/', [CartController::class, 'getCart'])->name('cart.list');
+    Route::post('/add', [CartController::class, 'addToCart'])->name('cart.add');
+    Route::put('/update', [CartController::class, 'updateCart'])->name('cart.update');
+    Route::delete('/remove/{id}', [CartController::class, 'removeCart'])->name('cart.remove');
 });
 
-Route::prefix('transaction')->group(function () {
-    Route::get('/', [TransactionController::class, 'getTransactionHistory'])->name('transaction.history')->middleware('auth');
-    Route::get('/detail/{id}', [TransactionController::class, 'getTransactionHistoryDetail'])->name('transaction.historyDetail')->middleware('auth');
-    Route::post('/checkout', [TransactionController::class, 'checkout'])->name('transaction.checkout')->middleware('auth');
+Route::prefix('transaction')->middleware('auth')->group(function () {
+    Route::get('/', [TransactionController::class, 'getTransactionHistory'])->name('transaction.history');
+    Route::get('/detail/{id}', [TransactionController::class, 'getTransactionHistoryDetail'])->name('transaction.historyDetail');
+    Route::post('/checkout', [TransactionController::class, 'checkout'])->name('transaction.checkout');
 });
 
-Route::prefix('profile')->group(function () {
+Route::prefix('profile')->middleware('auth')->group(function () {
     Route::get('/', [ProfileController::class, 'getProfile'])->name('profile.get');
     Route::put('/update-profile', [ProfileController::class, 'updateProfile'])->name('profile.updateProfile');
     Route::put('/update-password', [ProfileController::class, 'updatePassword'])->name('profile.updatePassword');
 });
-
-
-// Route::get('/update-game', function () {
-//     return view('AdminMenu/updateGame');
-// });
-
-// Route::get('/add-game', function () {
-//     return view('AdminMenu/addGame');
-// });
-
-// Route::get('/manage-game-genre', function () {
-//     return view('AdminMenu/manageGameGenre');
-// });
-
-// Route::get('/update-game-genre', function () {
-//     return view('AdminMenu/updateGameGenre');
-// });
-
-// Route::get('/profile', function () {
-//     return view('profile');
-// });
-
-// Route::get('/transaction-history-detail', function () {
-//     return view('transactionHistDetail');
-// });
